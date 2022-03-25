1f118b53dfd1cd3ec9f54cd44a783065358e332c
AdeebaSultana <adizaan8892@gmail.com>
2022-03-25 20:05:39 +0530

    Added Programs

---
+++ src/com/blz/FirstProgram.java
@@ -0,0 +1,23 @@
+package com.blz;
+
+public class FirstProgram {
+
+    public static void main(String[] args) {
+	//dataType varName = value;
+        short c = 12232;
+        int b = 1012345677;
+        long a = 1012345677823l;
+        float f = 12.20f;
+        double d = 12.20;
+
+        byte b1 = 'a';
+        char ch1 = 'a';
+        char ch2 = 'A';
+
+        int aCh1 = ch1;
+        int ACh = ch2;
+        System.out.println(ch1 + " : " + aCh1);
+        System.out.println(ch2 + " : " + ACh);
+        boolean flag = false;
+    }
+}
